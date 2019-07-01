01c6b577df262417b75ee00806b9f568e500e43c
jason <jf7776@gmail.com>
2019-06-30 18:33:51 -0700

    Initial router setup

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3005;
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const routes = require('./router/routes');
+app.use('/', routes);
+
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`)
+});
---
+++ router/routes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/users", (req, res) => {
+
+});
+
+router.get("/passes", (req, res) => {
+
+});
